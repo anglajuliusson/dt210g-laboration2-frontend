6c54a34a81f70920079b2fa61a958a9fd9f28ca1
Ängla Juliusson <angla02@icloud.com>
2026-01-29 12:56:30 +0100

    Importera index.css-filen i App.tsx

--- todos/src/App.tsx
+++ todos/src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header" // Importera Header-komponent
 import Footer from "./components/Footer" // Importera Footer-komponent
+import './index.css' // Importera index.css-filen
 
 function App() {
 
