2c7d7840bd4bd11d1990387e32815fa197cf1d89
Ängla Juliusson <angla02@icloud.com>
2026-01-29 14:44:46 +0100

    Importera och skriv ut TodoList-komponenten i App.tsx

--- todos/src/App.tsx
+++ todos/src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header" // Importera Header-komponent
 import Footer from "./components/Footer" // Importera Footer-komponent
 import './index.css' // Importera index.css-filen
+import TodoList from "./components/TodoList" // Importera TodoList-kompponent
 
 function App() {
 
   return (
     <>
       <Header />
+      <TodoList />
       <Footer />
     </>
   )
