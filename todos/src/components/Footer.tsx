64b7ae5eca178dd86a8fa632d6f70993e1764d57
Ängla Juliusson <angla02@icloud.com>
2026-01-29 13:05:27 +0100

    Inline-css för footer

--- todos/src/components/Footer.tsx
+++ todos/src/components/Footer.tsx
@@ -1,7 +1,14 @@
+// Inline-styling för header (komponentspecifik CSS)
+const footerStyle = {
+    backgroundColor: "rgb(142, 182, 195)",
+    color: "white",
+    padding: "3rem",
+}
+
 function Footer() {
     return (
-        <footer>
-            <p>DT210G - Laboration 2 | anju2402, Ängla Juliusson | VT26</p>
+        <footer style={footerStyle}>
+            <p style={{ textAlign: "center" }}>DT210G - Laboration 2 | anju2402, Ängla Juliusson | VT26</p>
         </footer>
     )
 }
