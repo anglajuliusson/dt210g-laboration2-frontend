f38c4a84f021c57487f87ad3551b93caa2e475c9
Ängla Juliusson <angla02@icloud.com>
2026-01-30 12:19:02 +0100

    Importera och skriv ut TodoForm-komponenten i App.tsx

--- todos/src/App.tsx
+++ todos/src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header" // Importera Header-komponent
 import Footer from "./components/Footer" // Importera Footer-komponent
 import './index.css' // Importera index.css-filen
 import TodoList from "./components/TodoList" // Importera TodoList-kompponent
+import TodoForm from "./components/TodoForm" // Importera TodoForm-komponent
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <TodoList />
+      <TodoForm />
       <Footer />
     </>
   )
