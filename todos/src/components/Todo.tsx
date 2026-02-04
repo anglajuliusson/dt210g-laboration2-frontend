04495978042c4da3191738f991cdf0bdae57b9a5
Ängla Juliusson <angla02@icloud.com>
2026-02-04 18:34:23 +0100

    Lägg till möjlighet att uppdatera status för todo

--- todos/src/components/Todo.tsx
+++ todos/src/components/Todo.tsx
@@ -17,7 +17,16 @@ const buttonStyle = {
     backgroundColor: "crimson",
     color: "white",
     cursor: "pointer",
-  };
+};
+
+// Styling för uppdatera status
+const selectStyle = {
+    marginTop: "0.6em",
+    padding: "0.5em",
+    borderRadius: "6px",
+    border: "1px solid #ccc",
+    margin: "1em"
+};
 
 // Todo-komponenten ansvarar för att visa information om en enskild todo
 // Komponenten tar emot ett todo-objekt via props
@@ -39,12 +48,50 @@ function Todo({todoProp} : {todoProp: TodoInterface}) {
         window.dispatchEvent(new Event("todos:refresh"));
     };
 
+    const statusArr = ["Ej påbörjad", "Pågående", "Avklarad"];
+
+    // Uppdaterar status på en todo
+    const updateStatus = async (id: number, status: string) => {
+      const resp = await fetch(`http://localhost:3000/todos/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            title: todoProp.title,
+            description: todoProp.description ?? "",
+            status,
+          }),
+      });
+  
+      if (!resp.ok) {
+        const msg = await resp.text();
+        console.log("Fel vid status-uppdatering:", msg);
+        throw new Error(msg);
+      }
+  
+      // Uppdatera listan direkt
+      window.dispatchEvent(new Event("todos:refresh"));
+    };
+
     return (
         // Varje todo renderas som ett list-element
         <li style={listStyle}>
             <h3>{todoProp.title}</h3> {/* Visar titel för todo */}
             <p>{todoProp.description}</p> {/* Visar beskrivning för todo */}
             <p>Status: {todoProp.status}</p> {/* Visar aktuell status för todo */}
+            <label>
+                Uppdatera status:
+                <select
+                style={selectStyle}
+                value={todoProp.status}
+                onChange={(e) => updateStatus(todoProp.id, e.target.value)}
+                >
+                {statusArr.map((s) => (
+                    <option key={s} value={s}>
+                    {s}
+                    </option>
+                ))}
+                </select>
+            </label>
             <button
                 style={buttonStyle}
                 onClick={() => {
